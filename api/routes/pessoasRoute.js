c478ca7b1f25419247f62f1065d9b2b5af1af6b1
Thomas Rodrigues <thomasnicola@outlook.com>
2022-04-28 14:54:41 -0300

    "CRUD"

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const PessoaController = require("../controllers/PessoaController.js");
 
-const router = Router()
+const router = Router();
 
-router.get("/pessoas", PessoaController.pegaTodaAsPessoas)
+router.get("/pessoas", PessoaController.pegaTodaAsPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
 
 module.exports = router;
